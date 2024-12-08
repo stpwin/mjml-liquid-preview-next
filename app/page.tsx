c8d3d869162253febcda9c67c0d67e337059a1a1
Kok Wee <kokwee.loh@ascenda.com>
2024-12-08 23:00:04 +0800

    Remove redundant web vitals

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-"use client";
-
 import { PreviewBuilder } from "@/components/previewer/preview-builder";
-import { useReportWebVitals } from "next/web-vitals";
-import { reportWebVitals } from "@/lib/analytics";
 
 export default function Page() {
-  useReportWebVitals(reportWebVitals);
-
   return <PreviewBuilder />;
 }
