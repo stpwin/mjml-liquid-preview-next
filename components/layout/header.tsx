dff3ac97136bf0a0ea10ea11166440d2f0351f9a
Kok Wee <kokwee.loh@ascenda.com>
2024-12-08 17:18:35 +0800

    Add theme

---
+++ components/layout/header.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { ThemeSwitcher } from "@/components/theme/theme-switcher"
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center">
+        <div className="mr-4 flex">
+          <a className="mr-6 flex items-center space-x-2" href="/">
+            <span className="font-bold sm:inline-block">
+              MJML Preview
+            </span>
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          <div className="w-full flex-1 md:w-auto md:flex-none">
+            {/* Add other header items here */}
+          </div>
+          <nav className="flex items-center">
+            <ThemeSwitcher />
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+} 
